92f1ae0e73567128f85346d9831c24c226e985a7
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-16 21:23:52 +0530

    [Shubham]Added: Demonstrate file operations and added test file

---
+++ src/test/java/com/capgi/NIOFileAPITest.java
@@ -0,0 +1,49 @@
+package com.capgi;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+public class NIOFileAPITest {
+	private static String HOME = "F:\\Capgemini_training1\\java_eclipse\\EmployeePayrollService";
+	private static String PLAY_WITH_NIO = "TempPlayGround";
+
+	@Test
+	public void givenPathWhenCheckedThenConfirm() throws IOException {
+		Path homePath = Paths.get(HOME);
+		assertTrue(Files.exists(homePath));
+		System.out.println(homePath);
+
+		Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+		if (Files.exists(playPath))
+			FileUtils.deleteFiles(playPath.toFile());
+		assertTrue(Files.notExists(playPath));
+
+		Files.createDirectory(playPath);
+		assertTrue(Files.exists(playPath));
+
+		IntStream.range(1, 10).forEach(cntr -> {
+			Path tempFile = Paths.get(playPath + "/temp" + cntr);
+			assertTrue(Files.notExists(tempFile));
+			try {
+				Files.createFile(tempFile);
+			} catch (IOException e) {
+			}
+			assertTrue(Files.exists(tempFile));
+		});
+
+		System.out.println("Files.list");
+		Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
+		System.out.println("Files.newDirectory");
+		Files.newDirectoryStream(playPath).forEach(System.out::println);
+		System.out.println("Files.newDirectory with temp");
+		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().contains("temp"))
+				.forEach(System.out::println);
+	}
+}
