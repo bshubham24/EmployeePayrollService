57d8ccbeeecefcde1d2612044a58e8e064cda485
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-11-02 23:38:01 +0530

    [Shubham]Added: Ability to update database using statment query

--- src/test/java/com/capgi/EmployeePayrollServiceDBTest.java
+++ src/test/java/com/capgi/EmployeePayrollServiceDBTest.java
@@ -1,6 +1,7 @@
 package com.capgi;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
@@ -14,5 +15,14 @@ public class EmployeePayrollServiceDBTest {
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
 		List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
 		assertEquals(4, employeePayrollData.size());
+	}
+
+	@Test
+	public void whenSalaryIsUpdated_ShouldMatchInDBAndList() throws EmployeePayrollException {
+		EmployeePayrollService empPayRollService = new EmployeePayrollService();
+		List<EmployeePayrollData> empPayrollList = empPayRollService.readEmployeePayrollData(IOService.DB_IO);
+		empPayRollService.updateEmployeeSalary("shubham", 150000.0);
+		boolean result = empPayRollService.checkEmployeePayrollInSyncWithDB("shubham");
+		assertTrue(result);
 	}
 }
